d55414ed914f33499e24ffb2b6d55cb8aeb63fdc
Abhishek kumar <aasalex21c@gmail.com>
2024-03-12 16:14:18 +0530

    almost done

--- src/Components/EducationPlanner.jsx
+++ src/Components/EducationPlanner.jsx
@@ -6,6 +6,16 @@ export default function EducationPlanner(){
   // maintaining states
     let [stateSubjectName, updateStateSubjectName] = useState('');
     let [stateSubjectHours, updateStateSubjectHours] = useState('');
+    let [stateStatusMessage, updateStateStatusMessage] = useState ({
+      message: 'hi there i am cortana',
+      displayNone : 'displayNone',
+      type : 'Error',
+      classNames : {
+        error: 'text-red-300',
+        success: 'text-green-300'
+      }
+    });
+
     // setting initial state
     let initialState = localStorage.getItem ('alex21cEducationPlannerWebApp');
     if(initialState){
@@ -20,7 +30,7 @@ export default function EducationPlanner(){
         );
 
     useEffect(()=>{
-      console.log(stateDbSubjects,  Object.entries(stateDbSubjects));
+      // console.log(stateDbSubjects,  Object.entries(stateDbSubjects));
   
       localStorage.setItem ('alex21cEducationPlannerWebApp', JSON.stringify(stateDbSubjects));
 
@@ -37,6 +47,16 @@ export default function EducationPlanner(){
       return `${timestamp}-${random}`;
     }
 
+    let showErrorMsg = (msg)=>{
+      updateStateStatusMessage(previousState=>(
+        {
+          ...previousState,
+          message: msg,
+          displayNone: ''
+        }
+      ));
+    }
+
 
 let addSubjectAndHours = ()=>{
   // console.log('okay adding, wait!');
@@ -45,12 +65,44 @@ let addSubjectAndHours = ()=>{
 
   
   // console.log(key);
+// console.log('subject horus are ', typeof stateSubjectHours)
+  if(stateSubjectHours < 0 ){
+    // console.log('wait there is an error');
+    showErrorMsg(`do you really want negative hours, kindly provide hours from 0 to Infinity!`);
+    return;
+    // just return
 
+  }
+  else  if(stateSubjectName === '' ){
+    // console.log('wait there is an error');
+    showErrorMsg(`Kindly provide any Subject Name!`);
+    return;
+    // just return
 
+  }
+  else  if(stateSubjectHours === '' ){
+    // console.log('wait there is an error');
+    showErrorMsg(`Kindly provide some value for Hours from 0 to Infinity`);
+    return;
+    // just return
+
+  }
+  
+  else{
+    // hide the error
+    updateStateStatusMessage(previousState=>(
+      {
+        ...previousState,
+        displayNone: 'displayNone'
+      }
+    ));          
+  }
+  
+  
   updateStateDbSubjects((previousState)=>{
 
     let key = generateUniqueId();
-    console.log('key is' , key);
+    // console.log('key is' , key);
     return {
       ...previousState,
       [key] : {
@@ -76,20 +128,26 @@ let addSubjectAndHours = ()=>{
 
 // returning JSX  
   return (
-    <div className="border-2 border-slate-200 p-[2rem] w-[50rem] mt-[2rem] m-auto rounded-md flex flex-col gap-[2rem] text-[1.2rem] text-slate-200">
+    <div id='wrapperEducationPlannerWebApp' className="border-2 border-slate-200 p-[2rem] w-[50rem] mt-[2rem] m-auto rounded-md flex flex-col gap-[2rem] text-[1.2rem] text-slate-200">
       <header className="flex flex-col items-center gap-[1rem]">
         <h1 className="smallCaps text-center font-semibold text-[3rem]  text-slate-50">Education Planner Web-App</h1>
         <form className="flex flex-col gap-[1rem]  w-[100%]">
-          <input className="text-slate-900 transition focus:outline focus:outline-2 focus:outline-yellow-500 p-[.5rem]   rounded-md" type="text" placeholder="Subject" onChange={
+          <input required className="text-slate-900 transition focus:outline focus:outline-2 focus:outline-yellow-500 p-[.5rem]   rounded-md" type="text" placeholder="Subject" onChange={
             (e)=>{
               updateStateSubjectName(e.target.value);
             }
           }/>
-          <input className="text-slate-900 transition focus:outline focus:outline-2 focus:outline-yellow-500 p-[.5rem]    rounded-md" type="number" placeholder="Hours" onChange={
+          <input required className="text-slate-900 transition focus:outline focus:outline-2 focus:outline-yellow-500 p-[.5rem]    rounded-md" type="number" placeholder="Hours" onChange={
             (e)=>{
               updateStateSubjectHours(Number(e.target.value));
             }
           }/>
+        <div className={`${stateStatusMessage} ${stateStatusMessage.type.toLocaleLowerCase() === 'error' ? stateStatusMessage.classNames.error : stateStatusMessage.classNames.success} ${stateStatusMessage.displayNone} flex gap-[.5rem] items-center` }>
+          <span className="font-bold smallCaps text-[2rem]"
+          >{stateStatusMessage.type}:</span>
+          <span>{stateStatusMessage.message}</span>
+        </div>
+
           <button className="font-semibold flex gap-[1rem] items-center justify-center outline outline-2 outline-amber-50 bg-yellow-300 hover:bg-yellow-500 transition cursor-pointer p-[1rem] rounded-md hover:text-slate-50 text-slate-900 text-[1.5rem]" type="submit" onClick ={
             (e)=>{
               e.preventDefault();
@@ -102,7 +160,7 @@ let addSubjectAndHours = ()=>{
         </form>
       </header>
 
-<div>
+<div className="flex flex-col gap-[2rem]">
   {
     Object.entries(stateDbSubjects).map(([key, data]) => (
       <OutputLines 
--- src/Components/OutputLines.jsx
+++ src/Components/OutputLines.jsx
@@ -8,47 +8,56 @@ export default function OutputLines({keyAndData, updateStateDbSubjects, stateDbS
   let key = keyAndData[0];
   let data = keyAndData[1];
 
-  console.log(data, key);
+  
   
   return (
-    <div className="flex gap-[1rem] items-center ">
-      <span className="text-[1.8rem]">{data.subject}- {data.hours} hours</span>
-      <button data-key={key} className="font-semibold  outline outline-2 outline-amber-50 bg-green-500 hover:bg-green-300 transition cursor-pointer pt-[.5rem] rounded-full pb-[.5rem] p-[1rem]  hover:text-slate-900 text-slate-50 text-[1.5rem]" onClick={
-        (event)=>{
-          let key = (event.target.getAttribute('data-key'));
-          let updatedStateDbSubjects = { ...stateDbSubjects };
-          updatedStateDbSubjects[key]["hours"] += 1; // Update the hours
-          updateStateDbSubjects(updatedStateDbSubjects); // Send the updated state back to the parent
-        }
-      }>
-        <i className="fa-sharp fa-solid fa-plus" data-key={key}></i>
-      </button>
+    <div className="outputLine flex gap-[1rem] items-center border-b-[1px] border-yellow-100 pb-[2rem] ">
+      <h2 className=" text-[1.8rem]">{data.subject}- {data.hours} hours</h2>
+      <div className="flex gap-[1rem]">
 
-      <button data-key={key}  className="font-semibold  outline outline-2 outline-amber-50 bg-blue-500 hover:bg-blue-300 transition cursor-pointer pt-[.5rem] rounded-full pb-[.5rem] p-[1rem]  hover:text-slate-900 text-slate-50 text-[1.5rem]" 
-      onClick={
-        (event)=>{
-          let key = (event.target.getAttribute('data-key'));
-          let updatedStateDbSubjects = { ...stateDbSubjects };
-          updatedStateDbSubjects[key]["hours"] -= 1; // Update the hours
-          updateStateDbSubjects(updatedStateDbSubjects); // Send the updated state back to the parent
-        }
-      }
+        <button data-key={key} className="font-semibold  outline outline-2 outline-amber-50 bg-green-500 hover:bg-green-300 transition cursor-pointer pt-[.5rem] rounded-full pb-[.5rem] p-[1rem]  hover:text-slate-900 text-slate-50 text-[1.5rem]" onClick={
+          (event)=>{
+            let key = (event.target.getAttribute('data-key'));
+            let updatedStateDbSubjects = { ...stateDbSubjects };
+            updatedStateDbSubjects[key]["hours"] += 1; // Update the hours
+            updateStateDbSubjects(updatedStateDbSubjects); // Send the updated state back to the parent
+          }
+        }>
+          <i className="fa-sharp fa-solid fa-plus" data-key={key}></i>
+        </button>
 
-      >
-        <i data-key={key}  className="fa-regular fa-minus"></i>
-      </button>
-      <button className="font-semibold  outline outline-2 outline-amber-50 bg-red-500 hover:bg-red-300 transition cursor-pointer pt-[.5rem] rounded-full pb-[.5rem] p-[1rem]  hover:text-slate-900 text-slate-50 text-[1.5rem]" 
-            onClick={
-              (event)=>{
-                let key = (event.target.getAttribute('data-key'));
-                let updatedStateDbSubjects = { ...stateDbSubjects };
-                updatedStateDbSubjects[key]["hours"] -= 1; // Update the hours
-                updateStateDbSubjects(updatedStateDbSubjects); // Send the updated state back to the parent
+        <button data-key={key}  className="font-semibold  outline outline-2 outline-amber-50 bg-blue-500 hover:bg-blue-300 transition cursor-pointer pt-[.5rem] rounded-full pb-[.5rem] p-[1rem]  hover:text-slate-900 text-slate-50 text-[1.5rem]" 
+        onClick={
+          (event)=>{
+            let key = (event.target.getAttribute('data-key'));
+            let updatedStateDbSubjects = { ...stateDbSubjects };
+            if(updatedStateDbSubjects[key]["hours"] <=0){
+              // just return no need to decrease it
+              return;
+            }
+            updatedStateDbSubjects[key]["hours"] -= 1; // Update the hours
+
+            updateStateDbSubjects(updatedStateDbSubjects); // Send the updated state back to the parent
+          }
+        }
+
+        >
+          <i data-key={key}  className="fa-regular fa-minus"></i>
+        </button>
+        <button  data-key={key}  className="font-semibold  outline outline-2 outline-amber-50 bg-red-500 hover:bg-red-300 transition cursor-pointer pt-[.5rem] rounded-full pb-[.5rem] p-[1rem]  hover:text-slate-900 text-slate-50 text-[1.5rem]" 
+              onClick={
+                (event)=>{
+                  let key = (event.target.getAttribute('data-key'));
+                  let updatedStateDbSubjects = { ...stateDbSubjects };
+                  
+                  delete updatedStateDbSubjects[key];
+                  updateStateDbSubjects(updatedStateDbSubjects); // Send the updated state back to the parent
+                }
               }
-            }// write code for delete
-      >
-        <i className="fa-solid fa-trash"></i>
-      </button>
+        >
+          <i className="fa-solid fa-trash"  data-key={key} ></i>
+        </button>
+      </div>
     </div>
   );
 }
